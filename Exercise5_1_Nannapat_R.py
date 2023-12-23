85d24d2d7f26f6c7783937c8699110ac762d1e39
tae1161 <nannapat.ruthun@gmail.com>
2023-12-23 15:37:52 +0700

    ้้homeworl5.1

---
+++ Exercise5_1_Nannapat_R.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number1:"))
+num2 = int(input("Enter a number2:"))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
