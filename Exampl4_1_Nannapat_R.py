87dca9cc8c372130c22d8f94ea0a8c1d2dd5389f
tae1161 <nannapat.ruthun@gmail.com>
2023-12-23 13:28:03 +0700

    homework1

--- Exampl4_1_Nannapat_R.py
+++ Exampl4_1_Nannapat_R.py
@@ -1,1 +1,9 @@
-print("ee")
+Found_Eng = 89.9
+Gen_Bs = 80
+Inter_Com = 79.6
+Com_Program = 82.3
+print("*********** Your Score ***********")
+print("Foundation English               :",Found_Eng)
+print("General Business                 :",Gen_Bs)
+print("Introduction to Computer Systems :",Inter_Com)
+print("Computer Programming             :",Com_Program)
